68db4489b2fcc68c0b316dc4afbc83559ee49f71
William Welling <wwelling@library.tamu.edu>
2020-10-01 09:08:07 -0500

    smpt address must be a string

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,7 +69,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
-      :address => relay.tamu.edu,
+      :address => 'relay.tamu.edu',
       :port => 25,
       :enable_starttls_auto => false,
       :openssl_verify_mode => 'none'
