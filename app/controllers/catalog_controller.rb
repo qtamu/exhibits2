cd4d962dff9828feb1821f54ee71d2bdce6feffe
William Welling <wwelling@library.tamu.edu>
2020-10-16 15:39:49 -0500

    use grid index for gallery

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -9,7 +9,7 @@ class CatalogController < ApplicationController
     config.show.oembed_field = :oembed_url_ssm
     config.show.partials.insert(1, :oembed)
 
-    config.view.gallery.partials = [:index_header, :index]
+    config.view.gallery.partials = [:index_header, :grid_index]
     config.view.masonry.partials = [:index]
     config.view.slideshow.partials = [:index]
 
