762a37b3299c7e8d9b3d49bf8bcc950179a6edb4
William Welling <wwelling@library.tamu.edu>
2020-10-30 22:50:12 -0500

    enable available tinymce plugins

--- app/assets/javascripts/spotlight/blocks/wysiwyg_block.js
+++ app/assets/javascripts/spotlight/blocks/wysiwyg_block.js
@@ -35,15 +35,16 @@ SirTrevor.Blocks.Wysiwyg = (function() {
         selector: "div.tmce-" + wb.blockID,
         inline: false,
         inline_styles: true,
-        menubar: false,
         theme: 'silver',
+        menubar: 'file edit view insert format tools table help',
         plugins: [
           'advlist autolink lists link charmap print preview hr anchor pagebreak',
           'searchreplace wordcount visualblocks visualchars code',
-          'insertdatetime nonbreaking table contextmenu directionality',
-          'paste textcolor colorpicker textpattern codesample'
+          'insertdatetime nonbreaking table directionality',
+          'paste textpattern codesample quickbars emoticons',
+          'fullscreen image media template imagetools noneditable help'
         ],
-        toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link | forecolor backcolor | table codesample',
+        toolbar: 'undo redo | insert | styleselect | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | bullist numlist | outdent indent | link | forecolor backcolor removeformat | table codesample | pagebreak | charmap emoticons | fullscreen preview save print | insertfile image media anchor codesample',
         setup: function(editor) {
           editor.on("change", function() {
             wb.blockStorage.data.text = editor.getContent();
