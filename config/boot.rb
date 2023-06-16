720ad2582ecca66f511ea31de1f8c80c965f222d
Q <qsean@library.tamu.edu>
2023-06-16 16:36:55 -0500

    boot file update

--- config/boot.rb
+++ config/boot.rb
@@ -4,4 +4,5 @@ require 'bundler/setup' # Set up gems listed in the Gemfile.
 # require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
 require 'bootsnap/setup' if Rails.env.development?
 # require 'bootsnap/setup' if ENV['RAILS_ENV'] != 'build'
+require 'rails/all'
 
