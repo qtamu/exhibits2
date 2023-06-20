8f1d567aa557ab2809268e22018dcafb1654c7b4
Q <qsean@library.tamu.edu>
2023-06-20 09:04:27 -0500

    fixed uninitialized rails bug

--- Gemfile
+++ Gemfile
@@ -8,7 +8,7 @@ ruby '2.7.4'
 gem 'rails', '>= 6.1', '< 8'
 
 # Use sqlite3 as the database for Active Record
-# gem 'sqlite3', '~> 1.4'
+gem 'sqlite3', '~> 1.6', '>= 1.6.3'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- config/boot.rb
+++ config/boot.rb
@@ -2,7 +2,7 @@ ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
 # require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
-require 'bootsnap/setup' if Rails.env.development?
-# require 'bootsnap/setup' if ENV['RAILS_ENV'] != 'build'
+# require 'bootsnap/setup' if Rails.env.development?
+require 'bootsnap/setup' if ENV['RAILS_ENV'] != 'build'
 require 'rails/all'
 
